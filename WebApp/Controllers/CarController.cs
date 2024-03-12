afed93ab9791f286c729603f9badfbe7054f688b
USER <USER@DESKTOP-AR90IKG>
2024-03-12 05:20:01 -0700

    Add project files.

---
+++ WebApp/Controllers/CarController.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+
+namespace WebApp.Controllers
+{
+    public class CarController : Controller
+
+    {
+        private readonly AddDbContext _context;
+        public CarController(AddDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var carList=_context.Cars.ToList();
+            return View(carList);
+        }
+    }
+}
---
+++ WebApp/Data/AddDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+namespace WebApp.Data
+
+{
+    public class AddDbContext:DbContext
+    {
+        public AddDbContext(DbContextOptions<AddDbContext> options) : base(options) { }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
